23355ccce2c4cd9496840e002134474168152ddd
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-21 18:34:14 -0500

    Initial Commit

---
+++ Callbacks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Callbacks
+//
+//  Created by Edwin Cardenas on 21/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
