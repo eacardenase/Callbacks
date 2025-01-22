c533e90c5fec8195dff9f13bd080ec74be76ebc5
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-22 15:30:07 -0500

    feat: use block instead of helper method in notification center

--- Callbacks/main.m
+++ Callbacks/main.m
@@ -12,10 +12,16 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         BNRLogger *logger = [[BNRLogger alloc] init];
         
-        [[NSNotificationCenter defaultCenter] addObserver:logger
-                                                 selector:@selector(zoneChange:)
-                                                     name:NSSystemTimeZoneDidChangeNotification
-                                                   object:nil];
+//        [[NSNotificationCenter defaultCenter] addObserver:logger
+//                                                 selector:@selector(zoneChange:)
+//                                                     name:NSSystemTimeZoneDidChangeNotification
+//                                                   object:nil];
+        [[NSNotificationCenter defaultCenter] addObserverForName:NSSystemTimeZoneDidChangeNotification
+                                                          object:nil
+                                                           queue:nil
+                                                      usingBlock:^(NSNotification * _Nonnull notification) {
+            NSLog(@"The system time zone has changed!");
+        }];
         
         NSURL *url = [NSURL URLWithString:@"https://www.gutenberg.org/cache/epub/205/pg205.txt"];
         NSURLRequest *request = [NSURLRequest requestWithURL:url];
