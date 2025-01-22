68f4e5248aee965a5d47e85615cb796f46c3fac8
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-21 19:00:13 -0500

    feat: add logger class and timer object

---
+++ BNRLogger.h
@@ -0,0 +1,21 @@
+//
+//  BNRLogger.h
+//  Callbacks
+//
+//  Created by Edwin Cardenas on 21/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BNRLogger : NSObject
+
+@property (nonatomic) NSDate *lastTime;
+
+- (NSString *)lastTimeString;
+- (void)updateLastTime:(NSTimer *)t;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Callbacks/main.m
+++ Callbacks/main.m
@@ -6,9 +6,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRLogger.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        BNRLogger *logger = [[BNRLogger alloc] init];
+        
+        __unused NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:2.0
+                                                          target:logger
+                                                        selector:@selector(updateLastTime:)
+                                                        userInfo:nil
+                                                         repeats:YES];
+        
         [[NSRunLoop currentRunLoop] run];
     }
     return 0;
