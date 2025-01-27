09f2e518915d64f58dacffb7b1bf4523cb62cae9
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-26 19:11:08 -0500

    feat: manually notify of observed value changes

--- BNRLogger.m
+++ BNRLogger.m
@@ -33,7 +33,11 @@
 - (void)updateLastTime:(NSTimer *)t
 {
     NSDate *now = [NSDate date];
-    [self setLastTime:now];
+    
+    [self willChangeValueForKey:@"lastTime"];
+    _lastTime = now;
+    [self didChangeValueForKey:@"lastTime"];
+    
     NSLog(@"Just set time to %@", self.lastTimeString);
 }
 
